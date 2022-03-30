0048686e9ecf4b3f305893a7dc3edb932d2470e4
hamid-murambiwa <machipisajunior@gmail.com>
2022-03-30 17:59:15 +0200

    Fetch greeting message from api

---
+++ app/javascript/api.js
@@ -0,0 +1,32 @@
+const initState = {
+  greeting: '',
+};
+
+const greetingReducer = (state = initState, action) => {
+  switch (action.type) {
+  case 'FETCHING_GREETING':
+    return {
+      ...state,
+      loaded: true,
+    };
+    case 'COMPLETE_GREETING':
+      return {
+        ...state,
+        loaded: false,
+        greeting: action.payload.greeting,
+      };
+    default:  
+    return state;
+  }
+};
+export const fetchgreeting = () => async (dispatch) => {
+  dispatch({ type: 'FETCHING_GREETING' });
+  await fetch('/api/v1/greetings')
+    .then((response) => response.json())
+    .then((data) => {
+      dispatch({ type: 'COMPLETE_GREETING', payload: data });
+    })
+    .catch(() => dispatch({ type: 'FETCHING_GREETING' }));
+};
+
+export default greetingReducer;

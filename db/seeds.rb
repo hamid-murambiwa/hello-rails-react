7169d4124ec7384b1fcc8b1121c4af711c1f5dde
hamid-murambiwa <machipisajunior@gmail.com>
2022-03-30 17:55:35 +0200

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-greet1 = Greeting.create(greeting: "Hello there, welcome my react app")
-greet2 = Greeting.create(greeting: "Ola!, welcome my react app")
-greet3 = Greeting.create(greeting: "I hope you're doing well, welcome my react app")
-greet4 = Greeting.create(greeting: "I hope you're having a great week, welcome my react app")
-greet5 = Greeting.create(greeting: "Hi everyone, welcome my react app")
+greet_one = Greeting.create(greeting: 'Hello there, welcome to my rails-react app')
+greet_two = Greeting.create(greeting: 'Ola!, welcome to my rails-react app')
+greet_three = Greeting.create(greeting: "I hope you're doing well, welcome to my rails-react app")
+greet_four = Greeting.create(greeting: "I hope you're having a great week, welcome to my rails-react app")
+greet_five = Greeting.create(greeting: 'Hi everyone, welcome to my rails-react app')

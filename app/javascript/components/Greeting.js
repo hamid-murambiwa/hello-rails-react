d14588f5d221c4ca6375c4ca612f0b05ba5c5fd6
hamid-murambiwa <machipisajunior@gmail.com>
2022-03-30 17:53:59 +0200

    Create Greeting component

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,12 +1,17 @@
 import React from "react"
-import PropTypes from "prop-types"
-class Greeting extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-      </React.Fragment>
-    );
-  }
-}
+import { useSelector } from 'react-redux';
+
+const Greeting = () => {
+  const greeting = useSelector((state) => state.greetingReducer.greeting);
+
+  return (
+    <div>
+      <h1>Rails-React App</h1>
+      <p>
+        {greeting}
+      </p>
+    </div>
+  );
+};
 
 export default Greeting
